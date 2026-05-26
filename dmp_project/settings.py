976f8d924ad687a70874e794437f50f02d59a039
mercadeomarmaz-ops <mercadeo.marmaz@gmail.com>
2026-05-26 10:55:59 -0500

    Update settings.py

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -132,7 +132,7 @@ PUBLISHER_NAME = os.getenv("PUBLISHER", "DMP - FREE MUSIC CATALOGUE SOFTWARE")
 PUBLISHER_CODE = os.getenv("PUBLISHER_CODE", "000")
 
 # IPI Name # is required, issued by collecting societies
-PUBLISHER_IPI_NAME = os.getenv("PUBLISHER_IPI_NAME", "1286976193")
+PUBLISHER_IPI_NAME = os.getenv("PUBLISHER_IPI_NAME", "00803318077")
 # IPI Base # is rarely used, issued by collecting societies
 PUBLISHER_IPI_BASE = os.getenv("PUBLISHER_IPI_BASE", None)
 
